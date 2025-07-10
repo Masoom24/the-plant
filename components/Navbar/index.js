8f0b02530c8f56a436cf855985a0abc25569a4c8
Masoom24 <masoomyadav0901@gmail.com>
2025-07-11 01:37:08 +0530

    minor-changes

--- components/Navbar/index.js
+++ components/Navbar/index.js
@@ -3,6 +3,7 @@ import Link from "next/link";
 import React, { useState } from "react";
 import { useRouter } from "next/navigation";
 import Cart from "../card";
+import { RxCross2 } from "react-icons/rx";
 
 const Navbar = () => {
   const [isMenuOpen, setIsMenuOpen] = useState(false);
@@ -60,23 +61,35 @@ const Navbar = () => {
           {/* 🛒 Cart Icon */}
           <div
             onClick={() => setShowCart(true)} //  Open modal
-            className="btn btn-ghost btn-circle cursor-pointer"
+            className=" cursor-pointer hover:bg-[#A0C88B] rounded-full"
           >
-            <div className="indicator">
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                className="h-5 w-5 text-black"
-                fill="none"
-                viewBox="0 0 24 24"
-                stroke="currentColor"
+            <div
+              tabIndex={0}
+              role="button"
+              className="p-2 rounded-full focus:outline-none"
+            >
+              <div
+                role="button"
+                tabIndex={0}
+                className="p-2 rounded-full cursor-pointer focus:outline-none"
               >
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  strokeWidth="2"
-                  d="M3 3h2l.4 2M7 13h10l4-8H5.4M7 13L5.4 5M7 13l-2.293 2.293c-.63.63-.184 1.707.707 1.707H17m0 0a2 2 0 100 4 2 2 0 000-4zm-8 2a2 2 0 11-4 0 2 2 0 014 0z"
-                />
-              </svg>
+                <div className="indicator">
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    className="h-6 w-6 text-black"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    stroke="currentColor"
+                  >
+                    <path
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      strokeWidth="2"
+                      d="M3 3h2l.4 2M7 13h10l4-8H5.4M7 13L5.4 5M7 13l-2.293 2.293c-.63.63-.184 1.707.707 1.707H17m0 0a2 2 0 100 4 2 2 0 000-4zm-8 2a2 2 0 11-4 0 2 2 0 014 0z"
+                    />
+                  </svg>
+                </div>
+              </div>
             </div>
           </div>
 
@@ -95,7 +108,7 @@ const Navbar = () => {
               onClick={() => setShowCart(false)}
               className="absolute top-4 right-4 text-xl font-bold text-gray-700 hover:text-red-600"
             >
-              ✖
+              <RxCross2 />
             </button>
             <Cart /> {/*  Call your Cart component here */}
           </div>
